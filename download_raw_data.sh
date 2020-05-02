6e348ec8d747a2abac586a915c5391bae304e2d3
clancygreen <clancy.c.green@gmail.com>
2020-05-02 11:11:10 -0700

    Merging ez-dl without merging

--- download_raw_data.sh
+++ download_raw_data.sh
@@ -1,2 +1,11 @@
 . config.sh
-cat setup_files/raw_data_urls.txt | xargs -n 1 -P 6 wget -c -P $TMP_DATA/
+
+
+for type in "${types[@]}"; do
+  for (( year=$START; year<=$END; year++ )); do 
+    cat setup_files/raw_data_urls.txt | \
+        grep $type | \
+        grep $year | \
+        xargs -n 1 -P 6 wget -c -P $TMP_DATA/
+  done
+done
